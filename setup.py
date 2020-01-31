27a088c79330864113e124097c8a4717b92ed902
Dhia Abbassi <dhiaabbassi90@gmail.com>
2020-01-31 18:32:47 +0100

    Added setup.py file :package:

---
+++ setup.py
@@ -0,0 +1,66 @@
+import subprocess
+from pathlib import PurePath
+
+from setuptools import find_packages, setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+__version__ = "1.0.0"
+__author__ = "Dhia Abbassi"
+__email__ = "dhia.absi@gmail.com"
+__license__ = "Apache License 2.0"
+
+packages = find_packages(exclude=["examples"])
+base_dir = PurePath(__file__).parent
+
+pipenv_command = ["pipenv", "install", "--deploy", "--system"]
+pipenv_command_dev = ["pipenv", "install", "--dev", "--deploy", "--system"]
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+
+    def run(self):
+        subprocess.check_call(pipenv_command_dev)
+        develop.run(self)
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+
+    def run(self):
+        subprocess.check_call(pipenv_command)
+        install.run(self)
+
+
+with open(PurePath(base_dir, "README.md")) as f:
+    long_description = f.read()
+
+classifiers = (
+    "License :: OSI Approved :: Apache Software License",
+    "Development Status :: 4 - Beta",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Operating System :: OS Independent",
+    "Intended Audience :: Information Technology",
+    "Natural Language :: English",
+)
+
+setup(
+    name="avrokafka",
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
+    license=__license__,
+    url="https://github.com/DhiaTN/avrokafka-py",
+    description="A schema-registry aware avro serde (serializer/deserializer) for Apache Kafka",
+    long_description="\n" + long_description,
+    packages=packages,
+    setup_requires=["pipenv"],
+    cmdclass={"develop": PostDevelopCommand, "install": PostInstallCommand},
+    zip_safe=False,
+    keywords="avro,kafka,confluent,schema registry",
+    classifiers=classifiers,
+)
