88337c864ac11c986e4ab2f1ed3ea0f0f3a2f920
Dhia Abbassi <dhiaabbassi90@gmail.com>
2020-01-31 18:15:58 +0100

    Initial implementation of schema registry client and avroserde

---
+++ avrokafka/__init__.py
@@ -0,0 +1,3 @@
+"""AvroKafka"""
+
+__version__ = "0.0.1"
---
+++ avrokafka/exceptions.py
@@ -0,0 +1,38 @@
+import json
+
+
+class SchemaRegistryError(Exception):
+    """Base Schema Registry error"""
+
+    status_code = None
+
+    def __init__(self, error, message):
+        self.error = str(self.error)
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
+class SchemaRegistryUnkownError(Exception):
+    status_code = None
+
+    def __init__(self, error):
+        self.error = str(self.error)
+        self.message = "Unexpected SchemaRegistry Error"
+
+
+class SchemaNotFoundError(SchemaRegistryError):
+    status_code = 404
+
+    def __init__(self, error):
+        self.message = "Schema not found"
+        self.error = str(error)
+
+
+class SchemaNotRegistredError(SchemaRegistryError):
+    status_code = 404
+
+    def __init__(self, error):
+        self.message = "Schema not registred"
+        self.error = str(error)
