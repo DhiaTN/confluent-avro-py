c56c705fcde666d0f0358d9407babc31d1e20b1c
Dhia Abbassi <dhia.abbassi@daimler.com>
2020-02-10 11:13:54 +0100

    Supporting pep 517 :alembic:

--- confluent_avro/__init__.py
+++ confluent_avro/__init__.py
@@ -1,1 +1,7 @@
-"""An Avro SerDe implementation that integrates with the confluent schema registry and serializes and deserializes data according to the defined confluent wire format"""
+"""
+An Avro SerDe implementation that integrates with the confluent
+schema registry and serializes and deserializes data according
+to the defined confluent wire format
+"""
+
+__version__ = "1.4.0"
